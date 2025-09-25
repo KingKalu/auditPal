f13128c247e0c7326dd89f0430463ea3789cef96
Isaac Kalu <kaluisaac580@gmail.com>
2025-09-25 12:53:32 +0100

    first commit

---
+++ src/@types/index.d.ts
@@ -0,0 +1,6 @@
+import { UserDocument } from "../models/user.model";
+
+declare global {
+  namespace Express {
+    interface User extends UserDocument{_id?:any}  }
+}
---
+++ src/middlewares/loginLimiter.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import rateLimit from "express-rate-limit";
+
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 min
+  max: 5,
+  // message: 'Too many login attemps. please try again later',
+  message: async (req:Request, res:Response) => {
+    return {
+      status: 'error',
+      message: `Too many login attempts from this IP, please try again later`,
+      timestamp: new Date().toISOString(),
+      remainingTime: Math.ceil(
+        (15 * 60 * 1000 - (Date.now() % (15 * 60 * 1000))) / 1000
+      ),
+    };
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+export default loginLimiter;
